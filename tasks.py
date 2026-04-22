1be8e61048cf784c89e4b37110f9aba1a4c54bc3
Pedro Tejero <pedrotejero.tech@gmail.com>
2026-04-22 15:04:56 +0200

    download the raw traffic data

--- tasks.py
+++ tasks.py
@@ -1,4 +1,7 @@
 from robocorp.tasks import task
+from RPA.HTTP import HTTP
+
+http = HTTP()
 
 @task
 def produce_traffic_data():
@@ -6,13 +9,16 @@ def produce_traffic_data():
     Inhuman Insurance, Inc. Artificial Intelligence System automation.
     Produces traffic data work items.
     """
-    print("produce")
-
+    http.download(
+        url="https://github.com/robocorp/inhuman-insurance-inc/raw/main/RS_198.json",
+        target_file="output/traffic.json",
+        overwrite=True,
+    )
 
 @task
 def consume_traffic_data():
     """
-    Inhuman Insurance, Inc. Artificial Intelligence System automation.
-    Consumes traffic data work items.
+        Inhuman Insurance, Inc. Artificial Intelligence System robot.
+        Consumes traffic data work items.
     """
     print("consume")
