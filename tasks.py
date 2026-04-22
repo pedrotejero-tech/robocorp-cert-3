68e1f2027874a999318e007ca5dbf0d0cda48096
Pedro Tejero <pedrotejero.tech@gmail.com>
2026-04-22 16:13:07 +0200

    Handle business exceptions

--- tasks.py
+++ tasks.py
@@ -37,30 +37,32 @@ def produce_traffic_data():
 @task
 def consume_traffic_data():
     """
-    Inhuman Insurance, Inc. Artificial Intelligence System robot.
+    Inhuman Insurance, Inc. Artificial Intelligence System automation.
     Consumes traffic data work items.
     """
-    process_traffic_data()
-
-def process_traffic_data():
     for item in workitems.inputs:
         traffic_data = item.payload["traffic_data"]
         if len(traffic_data["country"]) == 3:
             status, return_json = post_traffic_data_to_sales_system(traffic_data)
             if status == 200:
-                item.done() 
-            else: 
+                item.done()
+            else:
                 item.fail(
                     exception_type="APPLICATION",
                     code="TRAFFIC_DATA_POST_FAILED",
                     message=return_json["message"],
                 )
-    
+        else:
+            item.fail(
+                exception_type="BUSINESS",
+                code="INVALID_TRAFFIC_DATA",
+                message=item.payload,
+            )
+
 def post_traffic_data_to_sales_system(traffic_data):
     url = "https://robocorp.com/inhuman-insurance-inc/sales-system-api"
     response = requests.post(url, json=traffic_data)
     return response.status_code, response.json()
-
 
 def load_traffic_data_as_table():
     json_data = json.load_json_from_file(TRAFFIC_JSON_FILE_PATH)
