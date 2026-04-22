52c35c4e9efe47db553164122dc93b147ad2927f
Pedro Tejero <pedrotejero.tech@gmail.com>
2026-04-22 16:09:13 +0200

    Handle failed responses

--- tasks.py
+++ tasks.py
@@ -46,14 +46,20 @@ def process_traffic_data():
     for item in workitems.inputs:
         traffic_data = item.payload["traffic_data"]
         if len(traffic_data["country"]) == 3:
-            status = post_traffic_data_to_sales_system(traffic_data)
+            status, return_json = post_traffic_data_to_sales_system(traffic_data)
             if status == 200:
-                item.done()
+                item.done() 
+            else: 
+                item.fail(
+                    exception_type="APPLICATION",
+                    code="TRAFFIC_DATA_POST_FAILED",
+                    message=return_json["message"],
+                )
     
 def post_traffic_data_to_sales_system(traffic_data):
     url = "https://robocorp.com/inhuman-insurance-inc/sales-system-api"
     response = requests.post(url, json=traffic_data)
-    return response.status_code
+    return response.status_code, response.json()
 
 
 def load_traffic_data_as_table():
