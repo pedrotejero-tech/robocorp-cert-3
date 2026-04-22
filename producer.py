6dac0accaa547811885eb6756a28c4738eb2a0bf
Pedro Tejero <pedrotejero.tech@gmail.com>
2026-04-22 16:18:57 +0200

    Code refactoring - Splitting each task into a different file

--- producer.py
+++ producer.py
@@ -1,4 +1,3 @@
-import requests
 from robocorp import workitems
 from robocorp.tasks import task
 from RPA.HTTP import HTTP
@@ -34,39 +33,11 @@ def produce_traffic_data():
     payloads = create_work_item_payloads(filtered_data)
     save_work_item_payloads(payloads)
 
-@task
-def consume_traffic_data():
-    """
-    Inhuman Insurance, Inc. Artificial Intelligence System automation.
-    Consumes traffic data work items.
-    """
-    for item in workitems.inputs:
-        traffic_data = item.payload["traffic_data"]
-        if len(traffic_data["country"]) == 3:
-            status, return_json = post_traffic_data_to_sales_system(traffic_data)
-            if status == 200:
-                item.done()
-            else:
-                item.fail(
-                    exception_type="APPLICATION",
-                    code="TRAFFIC_DATA_POST_FAILED",
-                    message=return_json["message"],
-                )
-        else:
-            item.fail(
-                exception_type="BUSINESS",
-                code="INVALID_TRAFFIC_DATA",
-                message=item.payload,
-            )
-
-def post_traffic_data_to_sales_system(traffic_data):
-    url = "https://robocorp.com/inhuman-insurance-inc/sales-system-api"
-    response = requests.post(url, json=traffic_data)
-    return response.status_code, response.json()
 
 def load_traffic_data_as_table():
     json_data = json.load_json_from_file(TRAFFIC_JSON_FILE_PATH)
     return table.create_table(json_data["value"])
+
 
 def filter_and_sort_traffic_data(data):
     max_rate = 5.0
@@ -76,6 +47,7 @@ def filter_and_sort_traffic_data(data):
     table.sort_table_by_column(data, YEAR_KEY, False)
     return data
 
+
 def get_latest_data_by_country(data):
     data = table.group_table_by_column(data, COUNTRY_KEY)
     latest_data_by_country = []
@@ -83,6 +55,7 @@ def get_latest_data_by_country(data):
         first_row = table.pop_table_row(group)
         latest_data_by_country.append(first_row)
     return latest_data_by_country
+
 
 def create_work_item_payloads(traffic_data):
     payloads = []
@@ -94,6 +67,7 @@ def create_work_item_payloads(traffic_data):
         )
         payloads.append(payload)
     return payloads
+
 
 def save_work_item_payloads(payloads):
     for payload in payloads:
